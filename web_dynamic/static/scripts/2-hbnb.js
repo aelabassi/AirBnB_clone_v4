eeb40af75429d1f590274085b8e6dfe597edae0e
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-09 18:47:07 +0100

    first request to the API

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    $(() => {
+        $.ajax({
+            url: 'http://0.0.0.0:5001/api/v1/status/',
+            type: 'GET',
+            success: function (data) {
+                if (data.status === 'OK') {
+                    $('#api_status').addClass('available');
+                }
+            }
+        })
+    })
+
+})
