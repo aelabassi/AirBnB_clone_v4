663f77105872e08329de358380903c2cf420e933
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-09 12:49:25 +0100

    select amenities

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+    const amenities = {};
+    $('input[type="checkbox"]').change(function () {
+        if ($(this).is(':checked')) {
+        amenities[$(this).data('id')] = $(this).data('name');
+        } else {
+        delete amenities[$(this).data('id')];
+        }
+        if (Object.values(amenities).length > 0) {
+        $('.amenities h4').text(Object.values(amenities).join(', '));
+        } else {
+        $('.amenities h4').html('&nbsp;');
+        }
+    });
+})
