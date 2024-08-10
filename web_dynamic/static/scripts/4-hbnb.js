e4b22b0d0448a78447f66270e23a08b3bada87b8
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-10 18:51:24 +0100

    post Places to the API on click

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,118 @@
+$(document).ready(() => {
+
+    $('button').click(() => {
+
+        $.ajax({
+            url: 'http://0.0.0.0:5001/api/v1/places_search/',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({}),
+            success: function (data) {
+                for (const place of data) {
+                    $('.places').append(
+                        `<article>
+                            <div class="title_box">
+                                <h2>${place.name}</h2>
+                                <div class="price_by_night">
+                                    $${place.price_by_night}
+                                </div>
+                            </div>
+                            <div class="information">
+                                <div class="max_guest">
+                                    <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+                                    <br />
+                                    ${place.max_guest} Guests
+                                </div>
+                                <div class="number_rooms">
+                                    <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+                                    <br />
+                                    ${place.number_rooms} Bedrooms
+                                </div>
+                                <div class="number_bathrooms">
+                                    <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+                                    <br />
+                                    ${place.number_bathrooms} Bathroom 
+                                </div>
+                            </div>
+                            <div class="description">
+                                ${place.description}
+                            </div>
+                        </article>`);
+                    }
+            }
+        })
+    });
+
+        $.ajax({
+            url: 'http://0.0.0.0:5001/api/v1/status/',
+            type: 'GET',
+            success: function (data) {
+                if (data.status === "OK") {
+                    console.log(data.status);
+                    $('#api_status').addClass('available');
+                    console.log("Class added ", $('#api_status').attr('class'));
+                }
+                else{
+                    $('div#api_status').removeClass('available');
+                }
+
+            }
+    });
+    $.ajax({
+        url: 'http://0.0.0.0:5001/api/v1/places_search/',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({}),
+        success: function (data) {
+            for (const place of data) {
+                $('.places').append(
+                    `<article>
+                        <div class="title_box">
+                            <h2>${place.name}</h2>
+                            <div class="price_by_night">
+                                $${place.price_by_night}
+                            </div>
+                        </div>
+                        <div class="information">
+                            <div class="max_guest">
+                                <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+                                <br />
+                                ${place.max_guest} Guests
+                            </div>
+                            <div class="number_rooms">
+                                <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+                                <br />
+                                ${place.number_rooms} Bedrooms
+                            </div>
+                            <div class="number_bathrooms">
+                                <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+                                <br />
+                                ${place.number_bathrooms} Bathroom 
+                            </div>
+                        </div>
+                        <div class="description">
+                            ${place.description}
+                        </div>
+                    </article>`);
+                }
+        }
+    })
+
+    $('input').change(() => {
+
+    const amenities = {};
+    $('input[type="checkbox"]').change(function () {
+        if ($(this).is(':checked')) {
+            amenities[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete amenities[$(this).data('id')];
+        }
+        if (Object.values(amenities).length > 0) {
+            $('.amenities h4').text(Object.values(amenities).join(', '));
+        } else {
+            $('.amenities h4').html('&nbsp;');
+        }
+    });
+});
+
+})
